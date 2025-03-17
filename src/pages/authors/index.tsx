250f111a10525ffc84ca272b95f6e5a08237ff01
Bryce Kwan <brycekwan@Bryces-Mac-mini.local>
2025-03-17 18:48:37 -0400

    add authors form

--- src/pages/authors/index.tsx
+++ src/pages/authors/index.tsx
@@ -1,11 +1,67 @@
 import { Container, Typography } from "@mui/material";
 import TopMenu from "@/components/TopMenu/TopMenu";
+import { Formik, Form, Field } from "formik";
+import { TextField, Button } from "@mui/material";
+import { NumericFormat } from "react-number-format";
 
 export default function Authors() {
   return (
     <Container>
       <TopMenu />
       <Typography variant="h1">Authors</Typography>
+      <Typography variant="body1" paddingBottom={2}>
+        This is a sample page for authors. It uses Formik and
+        react-number-format to comma separate the book count field. On submssion
+        the value of the formik form will be the float value of the entered
+        value and not the string value. The values can be inspected in the console.
+      </Typography>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          bookCount: "",
+        }}
+        onSubmit={(values) => {
+          console.log("Form Data:", values);
+        }}
+      >
+        {({ values, setFieldValue }) => (
+          <Form>
+            <div style={{ marginBottom: "16px" }}>
+              <Field
+                name="firstName"
+                as={TextField}
+                label="First Name"
+                fullWidth
+              />
+            </div>
+            <div style={{ marginBottom: "16px" }}>
+              <Field
+                name="lastName"
+                as={TextField}
+                label="Last Name"
+                fullWidth
+              />
+            </div>
+            <div style={{ marginBottom: "16px" }}>
+              <NumericFormat
+                name="bookCount"
+                value={values.bookCount}
+                customInput={TextField}
+                label="Book Count"
+                onValueChange={(values) => {
+                  setFieldValue("bookCount", values.floatValue);
+                }}
+                thousandSeparator
+                fullWidth
+              ></NumericFormat>
+            </div>
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 }
--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -33,7 +33,14 @@ export default function Books() {
     <Container>
       <TopMenu />
       <Typography variant="h1">Books</Typography>
-
+      <Typography variant="body1" paddingBottom={2}>
+        This page shows zustand in action and react query. React query will
+        fetch once in the network tab with each unique search term. Reusing the
+        search term will not cause a query but use the cache since the stale
+        time is set to 5 minutes. The search history will show the last set of
+        search terms used. It will persist even if navigating to the author page
+        and back.
+      </Typography>
       <form onSubmit={formik.handleSubmit}>
         <Box display="flex" alignItems="center">
           <Typography variant="h6" display="inline-block" paddingRight="10px">
